d433f923336ddcca94693e7bc0985ffe93e144cb
Megan Smith <megan@tokaypress.com>
2016-06-29 16:17:23 -0700

    (fix) server

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var path = require('path');
-// var webpack = require('webpack');
-// var webpackDevMiddleware = require('webpack-dev-middleware');
-// var webpackHotMiddleware = require('webpack-hot-middleware');
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
 var config = require('./../webpack.config.js');
 
 var app = express();
 
-// var compiler = webpack(config);
+var compiler = webpack(config);
 
-// app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
-// app.use(webpackHotMiddleware(compiler));
+app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
+app.use(webpackHotMiddleware(compiler));
 
-// app.use(express.static('./dist'));
 app.use('/static', express.static(__dirname + '/../client'));
 
 app.get('/', function (req, res) {
